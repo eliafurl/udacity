b5460bb33d7cd6097a36da884c0750ebcca1904b
elia.furlanis <elia.furlanis@fcagroup.com>
2019-12-16 08:42:52 +0100

    updated .gitignore

--- term1-projects/02-Advanced_Lane_Lines/my_project/main.py
+++ term1-projects/02-Advanced_Lane_Lines/my_project/main.py
@@ -1,12 +1,12 @@
 #imports
-from LaneLinesDetection import LaneLinesDetection 
+from LaneLinesDetection import LaneLinesDetection
 import cv2
- 
+
 def main():
-    
+
     display = True
     images_for_calibration_path = '../camera_cal/calibration*.jpg'
-    
+
     LLD = LaneLinesDetection(images_for_calibration_path, display=display)
 
     test_image_path = '../test_images/straight_lines1.jpg'
@@ -18,7 +18,7 @@ def main():
         cv2.imwrite('../output_images/test_image.jpg',test_image)
 
     LLD.process_image(test_image)
-    
+
     print("I'm DONE!")
 
 if __name__ == "__main__":
