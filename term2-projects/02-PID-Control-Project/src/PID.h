33b8e794da66f9f7e142cce86a396d4767add7d9
elia.furlanis <elia.furlanis@fcagroup.com>
2020-03-21 16:34:51 +0100

    Implemented PID controller

--- term2-projects/02-PID-Control-Project/src/PID.h
+++ term2-projects/02-PID-Control-Project/src/PID.h
@@ -1,5 +1,14 @@
 #ifndef PID_H
 #define PID_H
+#include <vector>
+struct TwiddleParameters
+{
+  bool active;
+  double tolerance;
+  double best_error;
+  int iteration;
+  std::vector<double> delta_p;
+};
 
 class PID {
  public:
@@ -17,7 +26,7 @@ class PID {
    * Initialize PID.
    * @param (Kp_, Ki_, Kd_) The initial PID coefficients
    */
-  void Init(double Kp_, double Ki_, double Kd_);
+  void Init(double Kp_, double Ki_, double Kd_, bool twiddle);
 
   /**
    * Update the PID error variables given cross track error.
@@ -31,6 +40,18 @@ class PID {
    */
   double TotalError();
 
+  /**
+   * @brief Compute the control action
+   * @output the steering control
+   */
+  double ComputeControl();
+
+  /**
+   * @brief Twiddle algorithm for PID coefficients tuning
+   *
+   */
+  void Twiddle(double error);
+
  private:
   /**
    * PID Errors
@@ -41,10 +62,21 @@ class PID {
 
   /**
    * PID Coefficients
-   */ 
+   */
   double Kp;
   double Ki;
   double Kd;
+  std::vector<double*> gains = {&Kp, &Ki, &Kd};
+
+  /**
+   * Steering actuator saturation
+   */
+  const double MIN_CONTROL = -1.0;
+  const double MAX_CONTROL = 1.0;
+  /**
+   * Twiddle parameters
+   */
+  TwiddleParameters tp;
 };
 
 #endif  // PID_H
--- term2-projects/02-PID-Control-Project/src/main.cpp
+++ term2-projects/02-PID-Control-Project/src/main.cpp
@@ -37,6 +37,12 @@ int main() {
   /**
    * TODO: Initialize the pid variable.
    */
+  double K_p_0 = 1.0;//0.30351;//1.0;
+  double K_d_0 = 1.0;//0.001;//1.0;
+  double K_i_0 = 1.0;//2.66123;//1.0;
+
+  bool twiddle = false;
+  pid.Init(K_p_0, K_d_0, K_i_0, twiddle);
 
   h.onMessage([&pid](uWS::WebSocket<uWS::SERVER> ws, char *data, size_t length, 
                      uWS::OpCode opCode) {
@@ -63,7 +69,9 @@ int main() {
            * NOTE: Feel free to play around with the throttle and speed.
            *   Maybe use another PID controller to control the speed!
            */
-          
+          pid.UpdateError(cte);
+          pid.Twiddle(cte);
+          steer_value = pid.ComputeControl();
           // DEBUG
           std::cout << "CTE: " << cte << " Steering Value: " << steer_value 
                     << std::endl;
