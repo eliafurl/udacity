3dd6d3dfc4566359f54ec3a71a681f9525788500
elia.furlanis <elia.furlanis@fcagroup.com>
2020-04-04 17:32:58 +0200

    First implementation of the Twiddle algorithm [WIP]

--- term2-projects/03-PID-Control-Project/src/PID.h
+++ term2-projects/03-PID-Control-Project/src/PID.h
@@ -1,82 +1,98 @@
 #ifndef PID_H
 #define PID_H
 #include <vector>
-struct TwiddleParameters
+
+enum TwiddleState
 {
-  bool active;
-  double tolerance;
-  double best_error;
-  int iteration;
-  std::vector<double> delta_p;
+    START,
+    INCREMENTING,
+    DECREMENTING
+};
+
+struct TwiddleResources
+{
+    bool active;
+    double tolerance;
+    double best_error;
+    int iteration;
+    int ignore_initial_iterations;
+    int index_param;
+    std::vector<double> delta_param;
+    TwiddleState state;
 };
 
 class PID {
- public:
-  /**
-   * Constructor
-   */
-  PID();
+    public:
+        /**
+         * Constructor
+         */
+        PID();
 
-  /**
-   * Destructor.
-   */
-  virtual ~PID();
+        /**
+         * Destructor.
+         */
+        virtual ~PID();
 
-  /**
-   * Initialize PID.
-   * @param (Kp_, Ki_, Kd_) The initial PID coefficients
-   */
-  void Init(double Kp_, double Ki_, double Kd_, bool twiddle);
+        /**
+         * Initialize PID.
+         * @param (Kp_, Ki_, Kd_) The initial PID coefficients
+         */
+        void Init(double Kp_, double Ki_, double Kd_, bool twiddle);
 
-  /**
-   * Update the PID error variables given cross track error.
-   * @param cte The current cross track error
-   */
-  void UpdateError(double cte);
+        /**
+         * Update the PID error variables given cross track error.
+         * @param cte The current cross track error
+         */
+        void UpdateError(double cte);
 
-  /**
-   * Calculate the total PID error.
-   * @output The total PID error
-   */
-  double TotalError();
+        /**
+         * Calculate the total average PID error.
+         * @output The total average PID error
+         */
+        double TotalAverageError(double cte, int n_iter);
 
-  /**
-   * @brief Compute the control action
-   * @output the steering control
-   */
-  double ComputeControl();
+        /**
+         * @brief Compute the control action
+         * @output the steering control
+         */
+        double ComputeControl();
 
-  /**
-   * @brief Twiddle algorithm for PID coefficients tuning
-   *
-   */
-  void Twiddle(double error);
+        /**
+         * @brief Twiddle algorithm for PID coefficients tuning
+         *
+         */
+        void Twiddle(double avrg_error);
 
- private:
-  /**
-   * PID Errors
-   */
-  double p_error;
-  double i_error;
-  double d_error;
+        /**
+         * Twiddle resources parameters
+         */
+        TwiddleResources twiddle;
 
-  /**
-   * PID Coefficients
-   */
-  double Kp;
-  double Ki;
-  double Kd;
-  std::vector<double*> gains = {&Kp, &Ki, &Kd};
+    private:
+        void goToNextParam();
 
-  /**
-   * Steering actuator saturation
-   */
-  const double MIN_CONTROL = -1.0;
-  const double MAX_CONTROL = 1.0;
-  /**
-   * Twiddle parameters
-   */
-  TwiddleParameters tp;
+        /**
+         * PID Errors
+         */
+        double p_error;
+        double i_error;
+        double d_error;
+
+        double total_error;
+
+        /**
+         * PID Coefficients
+         */
+        double Kp;
+        double Ki;
+        double Kd;
+        std::vector<double*> gains = {&Kp, &Ki, &Kd};
+
+        /**
+         * Steering actuator saturation
+         */
+        const double MIN_CONTROL = -1.0;
+        const double MAX_CONTROL = 1.0;
 };
 
 #endif  // PID_H
