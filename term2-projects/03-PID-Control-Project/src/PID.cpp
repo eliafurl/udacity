6fd1bfbb3bd1aa0462ec50fab4f7dac0f2e3d88e
elia.furlanis <elia.furlanis@fcagroup.com>
2020-04-04 17:32:58 +0200

    First implementation of the Twiddle algorithm [WIP]

--- term2-projects/03-PID-Control-Project/src/PID.cpp
+++ term2-projects/03-PID-Control-Project/src/PID.cpp
@@ -1,83 +1,161 @@
 #include "PID.h"
 #include <numeric>
 #include <iostream>
-/**
- * TODO: Complete the PID class. You may add any additional desired functions.
- */
 
 PID::PID() {}
 
 PID::~PID() {}
 
-void PID::Init(double Kp_, double Ki_, double Kd_, bool twiddle) {
-  /**
-   * TODO: Initialize PID coefficients (and errors, if needed)
-   */
-  this->p_error = 0.0;
-  this->i_error = 0.0;
-  this->d_error = 0.0;
+void PID::Init(double Kp_, double Ki_, double Kd_, bool twiddle)
+{
+    /**
+     * TODO: Initialize PID coefficients (and errors, if needed)
+     */
+    this->p_error = 0.0;
+    this->i_error = 0.0;
+    this->d_error = 0.0;
 
-  this->Kp = Kp_;
-  this->Ki = Ki_;
-  this->Kd = Kd_;
+    this->total_error = 0.0;
 
-  if (twiddle) {
-    this->tp = {
-      true,             //active
-      0.05,             //tolerance
-      0.0,              //best_error
-      0,                //iteration
-      {0.1, 0.1, 0.1}   //delta_p
-    };
-  }
+    this->Kp = Kp_;
+    this->Ki = Ki_;
+    this->Kd = Kd_;
 
-}
-
-void PID::UpdateError(double cte) {
-  /**
-   * TODO: Update PID errors based on cte.
-   */
-
-  this->d_error = cte - this->p_error; // differential error
-  this->p_error = cte;                // proportional error
-  this->i_error += cte;                // integral error
-
-}
-
-double PID::TotalError() {
-  /**
-   * TODO: Calculate and return the total error
-   */
-  return 0.0;  // TODO: Add your total error calc here!
-}
-
-double PID::ComputeControl(){
-  double control = - this->Kp * this->p_error - this->Kd * this->d_error - this->Ki * this->i_error;
-  // steering actuator saturation
-  if (control < this->MIN_CONTROL) {
-    control = this->MIN_CONTROL;
-  } else if (control > this->MAX_CONTROL) {
-    control = this->MAX_CONTROL;
-  }
-  return control;
-}
-
-void PID::Twiddle(double error) {
-  double sum_threshold = std::accumulate(this->tp.delta_p.begin(), this->tp.delta_p.end(), 0.0);
-  if (true) {//(sum_threshold < this->tp.tolerance) {
-    for (int i=0; i < this->gains.size(); i++) {
-        //double dp = this->tp.delta_p[i];
-        //*this->gains[i] += dp;
-      if (error < this->tp.best_error) {
-        this->tp.best_error = error;
-        
-      }
+    if (twiddle)
+    {
+        // this->twiddle = { true,           //active
+        //                 0.05,             //tolerance
+        //                 0.0,              //best_error
+        //                 0,                //iteration
+        //                 100,              //ignore_initial_iterations
+        //                 0,                //index_param
+        //                 {0.1, 0.1, 0.1}   //delta_param
+        //               };
+        this->twiddle.active = true;
+        this->twiddle.tolerance = 0.05;
+        this->twiddle.best_error = 0.0;
+        this->twiddle.iteration = 0;
+        this->twiddle.ignore_initial_iterations = 100;
+        this->twiddle.index_param = 0;
+        this->twiddle.delta_param = {0.1, 0.1, 0.1};
+        this->twiddle.state = START;
     }
-  }
 
+}
 
-  std::cout << "[Twiddle] PID gains :\n"
-            << "Kp: " << Kp << "\n"
-            << "Kd: " << Kd << "\n"
-            << "Ki: " << Ki << std::endl;
+void PID::UpdateError(double cte)
+{
+    //Update PID errors based on cte.
+
+    this->d_error = cte - this->p_error; // differential error
+    this->p_error = cte;                // proportional error
+    this->i_error += cte;                // integral error
+
+}
+
+double PID::TotalAverageError(double cte, int n_iter)
+{
+    if (n_iter >= this->twiddle.ignore_initial_iterations)
+    {
+        this->total_error += cte * cte;
+    }
+    return this->total_error/n_iter;
+}
+
+double PID::ComputeControl()
+{
+    double control = - (this->Kp * this->p_error) - (this->Kd * this->d_error) - (this->Ki * this->i_error);
+    // steering actuator saturation
+    if (control < this->MIN_CONTROL)
+    {
+      control = this->MIN_CONTROL;
+    }
+    else if (control > this->MAX_CONTROL)
+    {
+      control = this->MAX_CONTROL;
+    }
+    return control;
+}
+
+void PID::Twiddle(double avrg_error)
+{
+    double sum_threshold = std::accumulate(twiddle.delta_param.begin(), twiddle.delta_param.end(), 0.0);
+    if (sum_threshold > twiddle.tolerance)
+    {
+        switch (twiddle.state)
+        {
+            case START:
+            {
+                twiddle.best_error = avrg_error;
+                *(this->gains[twiddle.index_param]) += twiddle.delta_param[twiddle.index_param];
+                twiddle.state = INCREMENTING;
+                break;
+            }
+            case INCREMENTING:
+            {
+                if (avrg_error < twiddle.best_error)
+                {
+                    // incrementing work well, so we increment even more next time
+                    twiddle.best_error = avrg_error;
+                    twiddle.delta_param[twiddle.index_param] *= 1.1;
+                    this->goToNextParam();
+                    *(this->gains[twiddle.index_param]) += twiddle.delta_param[twiddle.index_param];
+                }
+                else
+                {
+                    // otherwise try in the opposite direction
+                    *(this->gains[twiddle.index_param]) -= 2 * twiddle.delta_param[twiddle.index_param];
+                    twiddle.state = DECREMENTING;
+                }
+                break;
+            }
+            case DECREMENTING:
+            {
+                if (avrg_error < twiddle.best_error)
+                {
+                    // decrementing work well, so we increment even more next time
+                    twiddle.best_error = avrg_error;
+                    twiddle.delta_param[twiddle.index_param] *= 1.1;
+                }
+                else
+                {
+                    // otherwise we reset the current parameter to its original value and
+                    // reduce the amount modification
+                    *(this->gains[twiddle.index_param]) += twiddle.delta_param[twiddle.index_param];
+                    twiddle.delta_param[twiddle.index_param] *= 0.9;
+                }
+                twiddle.state = INCREMENTING;
+                this->goToNextParam();
+                *(this->gains[twiddle.index_param]) += twiddle.delta_param[twiddle.index_param];
+                break;
+            }
+        }
+    }
+    else
+    {
+        twiddle.active = false;
+    }
+
+    this->total_error = 0.0;
+    std::cout << "\n[Twiddle iteration: " << twiddle.iteration << " ] " << "PID gains :\n"
+              << "Kp: " << this->Kp << "\n"
+              << "Ki: " << this->Ki << "\n"
+              << "Kd: " << this->Kd << "\n"
+              << "Best error = " << twiddle.best_error << "\n"
+              << "Current error = " << avrg_error << "\n"
+              <<std::endl;
+}
+
+void PID::goToNextParam()
+{
+    do
+    {
+        this->twiddle.index_param = (this->twiddle.index_param + 1) % this->twiddle.delta_param.size();
+        // continue until there is a delta_param that we want to change (>0)
+    }
+    while (this->twiddle.delta_param[this->twiddle.index_param] == 0);
+
+    if (this->twiddle.index_param == 0) {
+        ++this->twiddle.iteration;
+    }
 }
--- term2-projects/03-PID-Control-Project/src/main.cpp
+++ term2-projects/03-PID-Control-Project/src/main.cpp
@@ -30,60 +30,94 @@ string hasData(string s) {
   return "";
 }
 
+// Reset the simulator
+void reset_simulator(uWS::WebSocket<uWS::SERVER> ws) {
+  std::string msg = "42[\"reset\",{}]";
+  ws.send(msg.data(), msg.length(), uWS::OpCode::TEXT);
+}
+
+
 int main() {
   uWS::Hub h;
 
   PID pid;
-  /**
-   * TODO: Initialize the pid variable.
-   */
-  double K_p_0 = 1.0;//0.30351;//1.0;
-  double K_d_0 = 1.0;//0.001;//1.0;
-  double K_i_0 = 1.0;//2.66123;//1.0;
+  //Initialize the pid variable. (lecture p=0,2, i=0,004, d=3) (twiddle: p = 0.313373, i = 0.004, d = 2.81048)
+  double K_p_0 = 0.2;//0.30351;
+  double K_i_0 = 0.004;//0.001;
+  double K_d_0 = 3;//2.66123;
 
-  bool twiddle = false;
-  pid.Init(K_p_0, K_d_0, K_i_0, twiddle);
+  bool twiddle = true;
+  pid.Init(K_p_0, K_i_0, K_d_0, twiddle);
 
-  h.onMessage([&pid](uWS::WebSocket<uWS::SERVER> ws, char *data, size_t length, 
-                     uWS::OpCode opCode) {
+  int n_iteration = 0;
+
+  h.onMessage([&pid, &n_iteration](uWS::WebSocket<uWS::SERVER> ws, char *data, size_t length,
+                     uWS::OpCode opCode)
+  {
     // "42" at the start of the message means there's a websocket message event.
     // The 4 signifies a websocket message
     // The 2 signifies a websocket event
-    if (length && length > 2 && data[0] == '4' && data[1] == '2') {
+    if (length && length > 2 && data[0] == '4' && data[1] == '2')
+    {
       auto s = hasData(string(data).substr(0, length));
 
-      if (s != "") {
+      if (s != "")
+      {
         auto j = json::parse(s);
 
         string event = j[0].get<string>();
 
-        if (event == "telemetry") {
+        if (event == "telemetry")
+        {
           // j[1] is the data JSON object
           double cte = std::stod(j[1]["cte"].get<string>());
           double speed = std::stod(j[1]["speed"].get<string>());
           double angle = std::stod(j[1]["steering_angle"].get<string>());
           double steer_value;
-          /**
-           * TODO: Calculate steering value here, remember the steering value is
-           *   [-1, 1].
-           * NOTE: Feel free to play around with the throttle and speed.
-           *   Maybe use another PID controller to control the speed!
-           */
+
+          //Calculate steering value in range [-1, 1].
           pid.UpdateError(cte);
-          pid.Twiddle(cte);
           steer_value = pid.ComputeControl();
           // DEBUG
-          std::cout << "CTE: " << cte << " Steering Value: " << steer_value 
-                    << std::endl;
+          // std::cout << "CTE: " << cte << " Steering Value: " << steer_value
+          //           << std::endl;
 
           json msgJson;
           msgJson["steering_angle"] = steer_value;
+          // Constant throttle
           msgJson["throttle"] = 0.3;
+
           auto msg = "42[\"steer\"," + msgJson.dump() + "]";
-          std::cout << msg << std::endl;
+          //std::cout << msg << std::endl;
           ws.send(msg.data(), msg.length(), uWS::OpCode::TEXT);
+
+          // Perform PID parameter tuning if twiddle is active
+          if (pid.twiddle.active)
+          {
+            if (n_iteration > pid.twiddle.ignore_initial_iterations)
+            {
+              double avrg_error = pid.TotalAverageError(cte, n_iteration);
+              std::cout << "avrg_error = " << avrg_error << std::endl;
+              if (avrg_error > 0.01)
+              {
+                pid.Twiddle(avrg_error);
+                reset_simulator(ws);
+                n_iteration = 0.0;
+              }
+              else
+              {
+                n_iteration++;
+              }
+            }
+            else
+            {
+              n_iteration++;
+            }
+          }
         }  // end "telemetry" if
-      } else {
+      }
+      else
+      {
         // Manual driving
         string msg = "42[\"manual\",{}]";
         ws.send(msg.data(), msg.length(), uWS::OpCode::TEXT);
@@ -91,23 +125,28 @@ int main() {
     }  // end websocket message if
   }); // end h.onMessage
 
-  h.onConnection([&h](uWS::WebSocket<uWS::SERVER> ws, uWS::HttpRequest req) {
+  h.onConnection([&h](uWS::WebSocket<uWS::SERVER> ws, uWS::HttpRequest req)
+  {
     std::cout << "Connected!!!" << std::endl;
   });
 
-  h.onDisconnection([&h](uWS::WebSocket<uWS::SERVER> ws, int code, 
-                         char *message, size_t length) {
+  h.onDisconnection([&h](uWS::WebSocket<uWS::SERVER> ws, int code,
+                         char *message, size_t length)
+  {
     ws.close();
     std::cout << "Disconnected" << std::endl;
   });
 
   int port = 4567;
-  if (h.listen(port)) {
+  if (h.listen(port))
+  {
     std::cout << "Listening to port " << port << std::endl;
-  } else {
+  }
+  else
+  {
     std::cerr << "Failed to listen to port" << std::endl;
     return -1;
   }
-  
+
   h.run();
 }
